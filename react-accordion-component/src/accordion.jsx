31806abf4cbe75b9e2ae4fdcd98ab22c6b3e69ce
Quintin Russell <quinn.j.russell@gmail.com>
2021-10-22 21:04:14 -0700

    added preliminary planning

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,8 +1,49 @@
 // import React from 'react';
 
+// const headers = [
+//   {
+//     'CSS': 'CSS is awesome, but sometimes frustrating',
+//     'key': '01'
+//     },
+//     {
+//       'HTML' : 'HTML is cool',
+//       'key': '02'
+//     },
+//     {
+//       'JavaScript' : 'JS is also really cool'
+//       'key': '03'
+//     }
+//   ]
+
+// class ContentCont extends React.Component {
+
+//   render() {
+//     return (
+//       <div>
+//         <p expand={this.props.expand}></p>
+//       </div>
+//     )
+//   }
+// }
+
 // export default class Accordion extends React.Component {
 //   constructor(props) {
 //     super(props);
+//     this.state = {
+//       clicked = null;
+//     }
+//   }
 
+//   handleClick(e) {
+//     const tar = e.target.data;
+//     this.setState({ clicked: tar })
+//   }
+
+//   expand()
+
+//   render(){
+//     return (
+//       <ContentCont expand={this.} />
+//     )
 //   }
 // }
